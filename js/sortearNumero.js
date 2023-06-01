c926ecdc9480bf9e8c1e2ac851b180c9e53d4e80
RudiJr <rudi.juniior@gmail.com>
2023-06-01 16:42:39 -0300

    Starting the js for the project

--- js/sortearNumero.js
+++ js/sortearNumero.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const menorValor = 1;
+const maiorValor = 1000;
+const numeroSecreto = gerarNumero();
+
+function gerarNumero(){
+    return parseInt(Math.random() * maiorValor + 1);
+};
+
+console.log("Número Secreto:", numeroSecreto);
+
+const elementoMenor = document.getElementById('menor-valor');
+elementoMenor.innerHTML = menorValor;
+
+const elementoMaior = document.getElementById('maior-valor');
+elementoMaior.innerHTML = maiorValor;
